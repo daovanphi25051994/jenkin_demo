5e50e4c18d846dea1a816f1b13bf05fe471e9ac7
daovanphi <daovanphi25051994@gmail.com>
2021-11-20 14:13:30 +0700

    demo

---
+++ src/main/java/com/example/testjenkind/TestJenkindApplication.java
@@ -0,0 +1,13 @@
+package com.example.testjenkind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJenkindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestJenkindApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testjenkind/TestJenkindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testjenkind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJenkindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
